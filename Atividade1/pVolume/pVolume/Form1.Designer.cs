1fdb491528cfd5a796c90b16bd63da47b46e9460
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2021-08-31 11:28:06 -0300

    Atividade

---
+++ Atividade1/pVolume/pVolume/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace pVolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.lbRaio = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lbAltura = new System.Windows.Forms.Label();
+            this.lbVolume = new System.Windows.Forms.Label();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(190, 100);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(320, 20);
+            this.txtRaio.TabIndex = 0;
+            // 
+            // lbRaio
+            // 
+            this.lbRaio.AutoSize = true;
+            this.lbRaio.Font = new System.Drawing.Font("Arial", 10.5F);
+            this.lbRaio.Location = new System.Drawing.Point(120, 100);
+            this.lbRaio.Name = "lbRaio";
+            this.lbRaio.Size = new System.Drawing.Size(37, 16);
+            this.lbRaio.TabIndex = 1;
+            this.lbRaio.Text = "Raio";
+            this.lbRaio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Arial", 12.5F);
+            this.btnCalc.Location = new System.Drawing.Point(123, 340);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(100, 50);
+            this.btnCalc.TabIndex = 3;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(190, 180);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(320, 20);
+            this.txtAltura.TabIndex = 2;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 12.5F);
+            this.btnLimpar.Location = new System.Drawing.Point(300, 340);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(100, 50);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Arial", 12.5F);
+            this.btnSair.Location = new System.Drawing.Point(480, 340);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(100, 50);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lbAltura
+            // 
+            this.lbAltura.AutoSize = true;
+            this.lbAltura.Font = new System.Drawing.Font("Arial", 10.5F);
+            this.lbAltura.Location = new System.Drawing.Point(120, 180);
+            this.lbAltura.Name = "lbAltura";
+            this.lbAltura.Size = new System.Drawing.Size(45, 16);
+            this.lbAltura.TabIndex = 6;
+            this.lbAltura.Text = "Altura";
+            this.lbAltura.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbVolume
+            // 
+            this.lbVolume.AutoSize = true;
+            this.lbVolume.Font = new System.Drawing.Font("Arial", 10.5F);
+            this.lbVolume.Location = new System.Drawing.Point(120, 260);
+            this.lbVolume.Name = "lbVolume";
+            this.lbVolume.Size = new System.Drawing.Size(54, 16);
+            this.lbVolume.TabIndex = 8;
+            this.lbVolume.Text = "Volume";
+            this.lbVolume.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(190, 260);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.ReadOnly = true;
+            this.txtVolume.Size = new System.Drawing.Size(320, 20);
+            this.txtVolume.TabIndex = 7;
+            this.txtVolume.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lbVolume);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.lbAltura);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lbRaio);
+            this.Controls.Add(this.txtRaio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.Label lbRaio;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lbAltura;
+        private System.Windows.Forms.Label lbVolume;
+        private System.Windows.Forms.TextBox txtVolume;
+    }
+}
+
---
+++ Atividade1/pVolume/pVolume/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pVolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Double raio = 0;
+            Double altura = 0;
+
+            if ((Double.TryParse(txtAltura.Text, out altura)) && (Double.TryParse(txtRaio.Text, out raio))){
+                raio = Double.Parse(txtRaio.Text);
+                altura = Double.Parse(txtAltura.Text);
+                txtVolume.Text = (Math.PI * Math.Pow(raio, 2) * altura).ToString("N2");
+            }
+            else
+            {
+                MessageBox.Show("Valores inválidos");
+                txtVolume.Clear();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtRaio.Clear();
+            txtVolume.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
