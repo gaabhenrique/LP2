15c384dce418df28e7561a8638c8de414dda1659
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2021-11-16 11:17:31 -0300

    Envio da prova pratica

---
+++ Prova/P0030482111007/P0030482111007/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace P0030482111007
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.lvContent = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(121, 126);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(211, 181);
+            this.btnVerify.TabIndex = 0;
+            this.btnVerify.Text = "Verificar";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // lvContent
+            // 
+            this.lvContent.HideSelection = false;
+            this.lvContent.Location = new System.Drawing.Point(483, 13);
+            this.lvContent.Name = "lvContent";
+            this.lvContent.Size = new System.Drawing.Size(305, 425);
+            this.lvContent.TabIndex = 1;
+            this.lvContent.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvContent);
+            this.Controls.Add(this.btnVerify);
+            this.Name = "frmPrincipal";
+            this.Text = "Prova Pratica";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.ListView lvContent;
+    }
+}
+
---
+++ Prova/P0030482111007/P0030482111007/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace P0030482111007
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+
+            lvContent.View = View.Details;
+            lvContent.LabelEdit = true;
+            lvContent.AllowColumnReorder = false;
+            lvContent.FullRowSelect = true;
+            lvContent.MultiSelect = false;
+            lvContent.GridLines = true;
+
+            lvContent.Columns.Add("Data", 150, HorizontalAlignment.Left);
+            lvContent.Columns.Add("Valor", 150, HorizontalAlignment.Left);
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            float[,] data = new float[7, 4];
+            string helper;
+
+            lvContent.Items.Clear();
+
+            for(int i = 0; i < data.GetLength(0); i++)
+            {
+                float monthValue = 0;
+
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    helper = Interaction.InputBox("Digite o valor referente ao mês " + (i + 1) + " Semana " + (j + 1),
+"Entrada de Dados");
+                    if(float.TryParse(helper, out data[i, j]))
+                    {
+                        ListViewItem item = new ListViewItem("Mês " + (i + 1) + " semana " + (j+1));
+                        item.SubItems.Add("RS" + data[i, j].ToString("N2"));
+                        lvContent.Items.Add(item);
+                        monthValue += data[i, j];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite um valor valido");
+                        j--;
+                    }
+                }
+
+                ListViewItem value = new ListViewItem("--> Total do mês " + i+1);
+                value.SubItems.Add("RS" + monthValue.ToString("N2"));
+                lvContent.Items.Add(value);
+                lvContent.Items.Add(new ListViewItem("---------------------------------------------------------"));
+
+            }
+        }
+    }
+}
